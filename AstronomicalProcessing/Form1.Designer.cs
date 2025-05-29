5b463b1351841986083da5978983b08964163955
Samuel Dunlop <P465735@tafe.wa.edu.au>
2025-05-29 13:18:34 +0800

    version 2.0

--- AstronomicalProcessing/Form1.Designer.cs
+++ AstronomicalProcessing/Form1.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             DataListBox = new ListBox();
-            SearchButton = new Button();
+            BinarySearchButton = new Button();
             SearchBox = new TextBox();
             EditBox = new TextBox();
             SortButton = new Button();
@@ -37,6 +38,12 @@
             SearchLabel = new Label();
             SaveButton = new Button();
             LoadButton = new Button();
+            MidExtremeButton = new Button();
+            ModeButton = new Button();
+            MeanButton = new Button();
+            RangeButton = new Button();
+            SequentialSearchButton = new Button();
+            toolTip1 = new ToolTip(components);
             SuspendLayout();
             // 
             // DataListBox
@@ -45,40 +52,43 @@
             DataListBox.ItemHeight = 15;
             DataListBox.Location = new Point(12, 12);
             DataListBox.Name = "DataListBox";
-            DataListBox.Size = new Size(281, 334);
+            DataListBox.Size = new Size(281, 394);
             DataListBox.TabIndex = 0;
+            toolTip1.SetToolTip(DataListBox, "Displays the currently loaded neutrino data");
             // 
-            // SearchButton
+            // BinarySearchButton
             // 
-            SearchButton.Location = new Point(299, 123);
-            SearchButton.Name = "SearchButton";
-            SearchButton.Size = new Size(104, 23);
-            SearchButton.TabIndex = 1;
-            SearchButton.Text = "Search";
-            SearchButton.UseVisualStyleBackColor = true;
-            SearchButton.Click += SearchButton_Click;
+            BinarySearchButton.Location = new Point(299, 152);
+            BinarySearchButton.Name = "BinarySearchButton";
+            BinarySearchButton.Size = new Size(113, 23);
+            BinarySearchButton.TabIndex = 1;
+            BinarySearchButton.Text = "Binary Search";
+            toolTip1.SetToolTip(BinarySearchButton, "Search the current data for the value in the search box using a binary search (data must be sorted first)");
+            BinarySearchButton.UseVisualStyleBackColor = true;
+            BinarySearchButton.Click += BinarySearchButton_Click;
             // 
             // SearchBox
             // 
             SearchBox.Location = new Point(299, 94);
             SearchBox.Name = "SearchBox";
-            SearchBox.Size = new Size(104, 23);
+            SearchBox.Size = new Size(113, 23);
             SearchBox.TabIndex = 3;
             // 
             // EditBox
             // 
-            EditBox.Location = new Point(299, 176);
+            EditBox.Location = new Point(299, 205);
             EditBox.Name = "EditBox";
-            EditBox.Size = new Size(104, 23);
+            EditBox.Size = new Size(113, 23);
             EditBox.TabIndex = 4;
             // 
             // SortButton
             // 
             SortButton.Location = new Point(299, 41);
             SortButton.Name = "SortButton";
-            SortButton.Size = new Size(104, 23);
+            SortButton.Size = new Size(113, 23);
             SortButton.TabIndex = 5;
             SortButton.Text = "Sort Data";
+            toolTip1.SetToolTip(SortButton, "Sort the current neutrino data from lowest to highest");
             SortButton.UseVisualStyleBackColor = true;
             SortButton.Click += SortButton_Click;
             // 
@@ -86,7 +96,7 @@
             // 
             EditLabel.AutoSize = true;
             EditLabel.ForeColor = SystemColors.ControlText;
-            EditLabel.Location = new Point(299, 158);
+            EditLabel.Location = new Point(299, 187);
             EditLabel.Name = "EditLabel";
             EditLabel.Size = new Size(108, 15);
             EditLabel.TabIndex = 6;
@@ -103,11 +113,12 @@
             // 
             // SaveButton
             // 
-            SaveButton.Location = new Point(299, 205);
+            SaveButton.Location = new Point(299, 234);
             SaveButton.Name = "SaveButton";
-            SaveButton.Size = new Size(104, 23);
+            SaveButton.Size = new Size(113, 23);
             SaveButton.TabIndex = 8;
             SaveButton.Text = "Save";
+            toolTip1.SetToolTip(SaveButton, "Replace the currently selected neutrino value with the value in the edit box");
             SaveButton.UseVisualStyleBackColor = true;
             SaveButton.Click += SaveButton_Click;
             // 
@@ -115,17 +126,78 @@
             // 
             LoadButton.Location = new Point(299, 12);
             LoadButton.Name = "LoadButton";
-            LoadButton.Size = new Size(104, 23);
+            LoadButton.Size = new Size(113, 23);
             LoadButton.TabIndex = 9;
             LoadButton.Text = "Load Data";
+            toolTip1.SetToolTip(LoadButton, "Load new neutrino data");
             LoadButton.UseVisualStyleBackColor = true;
             LoadButton.Click += LoadButton_Click;
+            // 
+            // MidExtremeButton
+            // 
+            MidExtremeButton.Location = new Point(299, 296);
+            MidExtremeButton.Name = "MidExtremeButton";
+            MidExtremeButton.Size = new Size(113, 23);
+            MidExtremeButton.TabIndex = 10;
+            MidExtremeButton.Text = "Find Mid-Extreme";
+            toolTip1.SetToolTip(MidExtremeButton, "Calculate and display the mid-extreme of the current data");
+            MidExtremeButton.UseVisualStyleBackColor = true;
+            MidExtremeButton.Click += MidExtremeButton_Click;
+            // 
+            // ModeButton
+            // 
+            ModeButton.Location = new Point(299, 325);
+            ModeButton.Name = "ModeButton";
+            ModeButton.Size = new Size(113, 23);
+            ModeButton.TabIndex = 11;
+            ModeButton.Text = "Find Mode";
+            toolTip1.SetToolTip(ModeButton, "Calculate and display the mode of the current data");
+            ModeButton.UseVisualStyleBackColor = true;
+            ModeButton.Click += ModeButton_Click;
+            // 
+            // MeanButton
+            // 
+            MeanButton.Location = new Point(299, 354);
+            MeanButton.Name = "MeanButton";
+            MeanButton.Size = new Size(113, 23);
+            MeanButton.TabIndex = 12;
+            MeanButton.Text = "Find Mean";
+            toolTip1.SetToolTip(MeanButton, "Calculate and display the mean of the current data");
+            MeanButton.UseVisualStyleBackColor = true;
+            MeanButton.Click += MeanButton_Click;
+            // 
+            // RangeButton
+            // 
+            RangeButton.Location = new Point(299, 383);
+            RangeButton.Name = "RangeButton";
+            RangeButton.Size = new Size(113, 23);
+            RangeButton.TabIndex = 13;
+            RangeButton.Text = "Find Range";
+            toolTip1.SetToolTip(RangeButton, "Calculate and display the mean of the current data");
+            RangeButton.UseVisualStyleBackColor = true;
+            RangeButton.Click += RangeButton_Click;
+            // 
+            // SequentialSearchButton
+            // 
+            SequentialSearchButton.Location = new Point(299, 123);
+            SequentialSearchButton.Name = "SequentialSearchButton";
+            SequentialSearchButton.Size = new Size(113, 23);
+            SequentialSearchButton.TabIndex = 14;
+            SequentialSearchButton.Text = "Sequential Search";
+            toolTip1.SetToolTip(SequentialSearchButton, "Search the current data for the value in the search box using a sequential search");
+            SequentialSearchButton.UseVisualStyleBackColor = true;
+            SequentialSearchButton.Click += SequentialSearchButton_Click;
             // 
             // AstronomicalProcessing
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(415, 357);
+            ClientSize = new Size(424, 417);
+            Controls.Add(SequentialSearchButton);
+            Controls.Add(RangeButton);
+            Controls.Add(MeanButton);
+            Controls.Add(ModeButton);
+            Controls.Add(MidExtremeButton);
             Controls.Add(LoadButton);
             Controls.Add(SaveButton);
             Controls.Add(SearchLabel);
@@ -133,7 +205,7 @@
             Controls.Add(SortButton);
             Controls.Add(EditBox);
             Controls.Add(SearchBox);
-            Controls.Add(SearchButton);
+            Controls.Add(BinarySearchButton);
             Controls.Add(DataListBox);
             Name = "AstronomicalProcessing";
             Text = "Astronomical Processing";
@@ -144,7 +216,7 @@
         #endregion
 
         private ListBox DataListBox;
-        private Button SearchButton;
+        private Button BinarySearchButton;
         private TextBox SearchBox;
         private TextBox EditBox;
         private Button SortButton;
@@ -152,5 +224,11 @@
         private Label SearchLabel;
         private Button SaveButton;
         private Button LoadButton;
+        private Button MidExtremeButton;
+        private Button ModeButton;
+        private Button MeanButton;
+        private Button RangeButton;
+        private Button SequentialSearchButton;
+        private ToolTip toolTip1;
     }
 }
--- AstronomicalProcessing/Form1.cs
+++ AstronomicalProcessing/Form1.cs
@@ -5,10 +5,11 @@
  * Author: Samuel Dunlop, Astrodata Refinement, Sprint One
  * Created: 22/5/2025
  * Modified: 27/5/2025
- * Version: 1.0.1
+ * Version: 2.0.0
  */
 
 using System.Drawing.Text;
+using System.Security.Cryptography;
 
 namespace AstronomicalProcessing
 {
@@ -16,7 +17,7 @@ namespace AstronomicalProcessing
     {
         // declare fields
         private int[] neutrinoValues = new int[24];
-        private bool dataIsSorted = false;
+        private bool dataIsSorted, dataIsLoaded = false;
 
         public AstronomicalProcessing()
         {
@@ -37,6 +38,7 @@ namespace AstronomicalProcessing
                 neutrinoValues[i] = rand.Next(10, 91);
             }
 
+            dataIsSorted = false;
             DisplayArray();
             MessageBox.Show("Neutrino data loaded.");
         }
@@ -60,64 +62,58 @@ namespace AstronomicalProcessing
         /// <param name="e"></param>
         private void SortButton_Click(object sender, EventArgs e)
         {
-            // declare variable for performing swaps
-            int temp;
-
-            // number of iterations is one less than array length because final value will already be sorted
-            for (int i = 1; i < neutrinoValues.Length; i++)
+            if (dataIsLoaded)
             {
-                // sort next highest value to end of unsorted values
-                for (int j = 0; j < neutrinoValues.Length - i; j++)
+                // declare variable for performing swaps
+                int temp;
+
+                // number of iterations is one less than array length because final value will already be sorted
+                for (int i = 1; i < neutrinoValues.Length; i++)
                 {
-                    // swap values if left value is higher
-                    if (neutrinoValues[j] > neutrinoValues[j + 1])
+                    // sort next highest value to end of unsorted values
+                    for (int j = 0; j < neutrinoValues.Length - i; j++)
                     {
-                        temp = neutrinoValues[j];
-                        neutrinoValues[j] = neutrinoValues[j + 1];
-                        neutrinoValues[j + 1] = temp;
+                        // swap values if left value is higher
+                        if (neutrinoValues[j] > neutrinoValues[j + 1])
+                        {
+                            temp = neutrinoValues[j];
+                            neutrinoValues[j] = neutrinoValues[j + 1];
+                            neutrinoValues[j + 1] = temp;
+                        }
                     }
                 }
-            }
 
-            dataIsSorted = true;
-            DisplayArray();
-            MessageBox.Show("Data sorted.");
+                dataIsSorted = true;
+                DisplayArray();
+                MessageBox.Show("Data sorted.");
+            }
+            else
+            {
+                MessageBox.Show("Please load some data first.");
+            }
         }
 
         /// <summary>
-        /// binary search function
+        /// sequential search function
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void SearchButton_Click(object sender, EventArgs e)
+        private void SequentialSearchButton_Click(object sender, EventArgs e)
         {
-            // check if data has been sorted before searching
-            if (dataIsSorted)
+            if (dataIsLoaded)
             {
                 // validate input
                 if (int.TryParse(SearchBox.Text, out int target))
                 {
-                    int min = 0;
-                    int max = neutrinoValues.Length;
-                    int mid;
 
-                    // binary search
-                    while (min < max)
+                    // sequential search
+                    for (int i = 0; i < neutrinoValues.Length; i++)
                     {
-                        mid = (min + max) / 2; // find middle value
-                        if (neutrinoValues[mid] == target)
+                        if (neutrinoValues[i] == target)
                         {
-                            DataListBox.SelectedIndex = mid;
-                            MessageBox.Show($"Value found at list location {mid + 1}.");
-                            return; // exit early if value found
-                        }
-                        else if (neutrinoValues[mid] > target) // select the left half of the array
-                        {
-                            max = mid - 1;
-                        }
-                        else // select the right half of the array
-                        {
-                            min = mid + 1;
+                            DataListBox.SelectedIndex = i;
+                            MessageBox.Show($"Value found at list location {i + 1}.");
+                            return;
                         }
                     }
 
@@ -130,7 +126,64 @@ namespace AstronomicalProcessing
             }
             else
             {
-                MessageBox.Show("Data must be sorted first.");
+                MessageBox.Show("Please load some data first.");
+            }
+        }
+
+        /// <summary>
+        /// binary search function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BinarySearchButton_Click(object sender, EventArgs e)
+        {
+            if (dataIsLoaded)
+            {
+                // check if data has been sorted before searching
+                if (dataIsSorted)
+                {
+                    // validate input
+                    if (int.TryParse(SearchBox.Text, out int target))
+                    {
+                        int min = 0;
+                        int max = neutrinoValues.Length;
+                        int mid;
+
+                        // binary search
+                        while (min < max)
+                        {
+                            mid = (min + max) / 2; // find middle value
+                            if (neutrinoValues[mid] == target)
+                            {
+                                DataListBox.SelectedIndex = mid;
+                                MessageBox.Show($"Value found at list location {mid + 1}.");
+                                return; // exit early if value found
+                            }
+                            else if (neutrinoValues[mid] > target) // select the left half of the array
+                            {
+                                max = mid - 1;
+                            }
+                            else // select the right half of the array
+                            {
+                                min = mid + 1;
+                            }
+                        }
+
+                        MessageBox.Show("Value not found.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid search term.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Data must be sorted first.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please load some data first.");
             }
         }
 
@@ -141,26 +194,171 @@ namespace AstronomicalProcessing
         /// <param name="e"></param>
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            // check the user has selected a value to edit
-            if (DataListBox.SelectedIndex != -1)
+            if (dataIsLoaded)
             {
-                // set the selected value to the new value
-                if (int.TryParse(EditBox.Text, out int newValue))
+                // check the user has selected a value to edit
+                if (DataListBox.SelectedIndex != -1)
                 {
-                    neutrinoValues[DataListBox.SelectedIndex] = newValue;
+                    // set the selected value to the new value
+                    if (int.TryParse(EditBox.Text, out int newValue))
+                    {
+                        neutrinoValues[DataListBox.SelectedIndex] = newValue;
 
-                    // data must be sorted again before search
-                    dataIsSorted = false;
-                    DisplayArray();
+                        // data must be sorted again before search
+                        dataIsSorted = false;
+                        DisplayArray();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid value first.");
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Please enter a valid value first.");
+                    MessageBox.Show("Please select a value to edit.");
                 }
             }
             else
             {
-                MessageBox.Show("Please select a value to edit.");
+                MessageBox.Show("Please load some data first.");
+            }
+        }
+
+        /// <summary>
+        /// calculates the mid-extreme of the neutrino data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MidExtremeButton_Click(object sender, EventArgs e)
+        {
+            if (dataIsLoaded)
+            {
+                int min = neutrinoValues[0], max = neutrinoValues[neutrinoValues.Length - 1];
+
+                // if 
+                if (!dataIsSorted)
+                {
+                    foreach (int neutrinoValue in neutrinoValues)
+                    {
+                        if (neutrinoValue < min)
+                        {
+                            min = neutrinoValue;
+                        }
+                        else if (neutrinoValue > max)
+                        {
+                            max = neutrinoValue;
+                        }
+                    }
+                }
+
+                MessageBox.Show($"The mid-extreme of the data is {(double)(min + max) / 2.0}");
+            }
+            else
+            {
+                MessageBox.Show("Please load some data first.");
+            }
+        }
+
+        /// <summary>
+        /// calculates the mode of the neutrino data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ModeButton_Click(object sender, EventArgs e)
+        {
+            if (dataIsLoaded)
+            {
+                // count is set to 1 so we can skip comparing each value to itself
+                int count = 1, maxCount = 0, mode = 0;
+
+                // traverse neutrino array, skipping last value
+                for (int i = 0; i < neutrinoValues.Length - 1; i++)
+                {
+                    // compare current value to all proceeding values
+                    for (int j = i + 1; j < neutrinoValues.Length; j++)
+                    {
+                        if (neutrinoValues[j] == neutrinoValues[i])
+                        {
+                            count++;
+                        }
+                    }
+
+                    // check if current value is mode, preferring lowest value
+                    if (count > maxCount | (count == maxCount && neutrinoValues[i] < mode))
+                    {
+                        maxCount = count;
+                        mode = neutrinoValues[i];
+                    }
+
+                    count = 1;
+                }
+
+                MessageBox.Show($"The mod of the data is {mode}, occurring {maxCount} times.");
+            }
+            else
+            {
+                MessageBox.Show("Please load some data first.");
+            }
+        }
+
+        /// <summary>
+        /// calculates the mean of the neutrino data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MeanButton_Click(object sender, EventArgs e)
+        {
+            if (dataIsLoaded)
+            {
+                int sum = 0;
+                double mean;
+
+                foreach (int neutrinoValue in neutrinoValues)
+                {
+                    sum += neutrinoValue;
+                }
+
+                mean = Double.Round((double)sum / (double)neutrinoValues.Length, 2);
+
+                MessageBox.Show($"The mean of the data is {mean}");
+            }
+            else
+            {
+                MessageBox.Show("Please load some data first.");
+            }
+        }
+
+        /// <summary>
+        /// calculates the range of the neutrino data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RangeButton_Click(object sender, EventArgs e)
+        {
+            if (dataIsLoaded)
+            {
+                int min = neutrinoValues[0], max = neutrinoValues[neutrinoValues.Length - 1];
+
+                if (!dataIsSorted)
+                {
+                    foreach (int neutrinoValue in neutrinoValues)
+                    {
+                        if (neutrinoValue < min)
+                        {
+                            min = neutrinoValue;
+                        }
+                        else if (neutrinoValue > max)
+                        {
+                            max = neutrinoValue;
+                        }
+                    }
+                }
+
+                MessageBox.Show($"The range of the data is {max - min}");
+            }
+            else
+            {
+                MessageBox.Show("Please load some data first.");
             }
         }
     }
